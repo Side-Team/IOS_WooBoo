84dd22e096411c976e8d7b5d8a28e79263725d23
ParkSeMi <qkrtpa12@naver.com>
2021-02-23 13:13:49 +0900

    21.02.23 / 박세미 => storyboard 80%

---
+++ WooBoo_Project/WooBoo_Project/LoginViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LoginViewController.swift
+//  WooBoo_Project
+//
+//  Created by ssemm on 2021/02/22.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func btnJoin(_ sender: UIButton) {
+        
+        
+        
+//        if let vc = self.storyboard?.instantiateViewController(identifier: "Main") as? JoinViewController{
+//            present(vc, animated: true, completion: nil)
+//        }
+//        let vc = self.storyboard?.instantiateViewController(withIdentifier: "JoinViewController") as! JoinViewController
+//
+//        present(vc, animated: true, completion: nil)
+        
+      //  performSegue(withIdentifier: "Show Join", sender: sender)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

ac515dc1c0156b76a0a26b60e9bed3ee2f838258
이민우 <tnctis21@naver.com>
2021-02-23 16:10:56 +0900

    2021-02-23 / 16:10 / 이민우 =>
    1. 스토리보드 정리
    2. 시작화면 로그인화면으로 설정

--- WooBoo_Project/WooBoo_Project/LoginViewController.swift
+++ WooBoo_Project/WooBoo_Project/LoginViewController.swift
@@ -15,28 +15,14 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func btnLogin(_ sender: UIButton) {
+        let vcName = self.storyboard?.instantiateViewController(withIdentifier: "MainView")
+        vcName?.modalPresentationStyle = .fullScreen
+        self.present(vcName!, animated: true, completion: nil)
+    }
     
     @IBAction func btnJoin(_ sender: UIButton) {
-        
-        
-        
-//        if let vc = self.storyboard?.instantiateViewController(identifier: "Main") as? JoinViewController{
-//            present(vc, animated: true, completion: nil)
-//        }
-//        let vc = self.storyboard?.instantiateViewController(withIdentifier: "JoinViewController") as! JoinViewController
-//
-//        present(vc, animated: true, completion: nil)
-        
-      //  performSegue(withIdentifier: "Show Join", sender: sender)
+        self.performSegue(withIdentifier: "MoveJoinUs", sender: self)
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

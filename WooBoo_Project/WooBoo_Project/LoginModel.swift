3c6acb6505ddd00a5d7898328268ada28a8cd9eb
ParkSeMi <qkrtpa12@naver.com>
2021-02-24 10:29:46 +0900

    21.02.24 / 10:30 / 박세미 => LoginModel 하는중

--- WooBoo_Project/WooBoo_Project/LoginModel.swift
+++ WooBoo_Project/WooBoo_Project/LoginModel.swift
@@ -14,33 +14,36 @@ protocol LoginModelProtocol: class{
 class LoginModel: NSObject{
     var delegate: LoginModelProtocol!
     var urlPath = "http://127.0.0.1:8080/iosjsp/loginCheck.jsp"
-//    
-//    func loginitem(uEmail: String, uPw: String) -> Bool{
-//        var result: Bool = true
-//        
-//        let urlAdd = "?uEmail=\(uEmail)&uPw=\(uPw)"
-//        
-//        urlPath = urlPath + urlAdd
-//        
-//        func downloadItems(){
-//            let url = URL(string: urlPath)!
-//            let defaultSession = Foundation.URLSession(configuration: URLSessionConfiguration.default)
-//            
-//            let task = defaultSession.dataTask(with: url){(data, responds, error) in
-//                if error != nil{
-//                    print("Failed to download data")
-//                }else{
-//                    print("Data is downloading")
-//                    self.parseJSON(data!)
-//                }
-//            }
-//            task.resume()
-//            return result
-//        }
-//    }
+    
+    func insertItems(email: String, pw: String) -> Bool{   // Bool은 됐는지 안됐는지 결과를 받기위함
+        var result: Bool = true
+        let urlAdd = "?uemail=\(email)&upw:\(pw)"    //jsp 뒤에 ? 내용은 이것을 통해서 데이터가 들어감
+        urlPath = urlPath + urlAdd
+        
+        // 한글 url encoding
+        urlPath = urlPath.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)!
+        
+        let url: URL = URL(string: urlPath)!
+        let defaultSession = Foundation.URLSession(configuration: URLSessionConfiguration.default)
+        
+        let task = defaultSession.dataTask(with: url){(data, response, error) in
+            if error != nil{
+                print("Failed to insert data")
+                result = false
+            }else{
+                print("Data is inserted!")
+                result = true
+            }
+        }
+        task.resume()   // 실행
+        return result
+        
+    }
   
     
     func parseJSON(_ data: Data){
+        
+        var resultValue:Int = 0
         var jsonResult = NSArray()
         
         do{
@@ -55,14 +58,7 @@ class LoginModel: NSObject{
         for i in 0..<jsonResult.count{
             
             jsonElement = jsonResult[i] as! NSDictionary    // jsonResult[i]를 NSDictionary로 바꿔준다.
-            let query = UserModel()
             
-            if let uEmail = jsonElement["Email"] as? String,
-               let uPw = jsonElement["PW"] as? String{
-                query.uEmail = uEmail
-                query.uPw = uPw
-            }
-            locations.add(query)
         }
         DispatchQueue.main.async(execute: {() -> Void in
             self.delegate.itemDownloaded(items: locations)

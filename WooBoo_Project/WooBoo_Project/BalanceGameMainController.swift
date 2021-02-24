e3e42acc73f887523ee8e8b33447f08e6f71de6f
gimbolam <3017129@naver.com>
2021-02-24 18:14:15 +0900

    2021.02.24 보람 = > 디비 연결 전

---
+++ WooBoo_Project/WooBoo_Project/BalanceGameMainController.swift
@@ -0,0 +1,34 @@
+//
+//  BalanceGameMainController.swift
+//  WooBoo_Project
+//
+//  Created by 김보람 on 2021/02/23.
+//
+
+import UIKit
+
+class BalanceGameMainController: UIViewController {
+
+    
+   
+    @IBOutlet weak var btnStart: UIButton!
+    
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+      
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ WooBoo_Project/WooBoo_Project/BalanceGameSelectController.swift
@@ -0,0 +1,78 @@
+//
+//  BalanceGameSelectController.swift
+//  WooBoo_Project
+//
+//  Created by 김보람 on 2021/02/24.
+//
+
+import UIKit
+
+class BalanceGameSelectController: UIViewController {
+    
+    @IBOutlet weak var lblTitle: UILabel!
+    @IBOutlet weak var btnSelect1: UIButton!
+    @IBOutlet weak var btnSelect2: UIButton!
+    @IBOutlet weak var lblSelect1percent: UILabel!
+    @IBOutlet weak var lblSelect2percent: UILabel!
+    @IBOutlet weak var lblSelect2Count: UILabel!
+    @IBOutlet weak var lblSelect1Count: UILabel!
+    
+    var btitle: String = ""
+    var select1: String = "바뀌나"
+    var select2: String = ""
+    var select1Percent : String = ""
+    var select1Count: String = ""
+    var select2Percent: String = ""
+    var select2Count: String = ""
+    var checkValue = -1
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //btnSelect1.setTitle(select1, for: UIControl.State.normal)
+        btnSelect1.layer.cornerRadius = 10
+        btnSelect2.layer.cornerRadius = 10
+    }
+    
+    @IBAction func btnSelect1(_ sender: UIButton) {
+        checkValue = 0
+        checkButtonStatus()
+    }
+    
+    @IBAction func btnSelect2(_ sender: UIButton) {
+        checkValue = 1
+        checkButtonStatus()
+    }
+    
+
+    
+    func checkButtonStatus(){
+
+            if checkValue == 0{
+                btnSelect1.backgroundColor = UIColor(red: 128/255, green: 194/255, blue: 179/255, alpha: 1)
+                btnSelect1.setTitleColor(UIColor.white, for: UIControl.State.normal)
+                btnSelect2.backgroundColor = UIColor.white
+                btnSelect2.setTitleColor(UIColor.black, for: UIControl.State.normal)
+                
+                // 여기에 버튼 클릭됐을때 밑에 퍼센트값이랑 카운트 보이고 안보이고넣기
+
+            }else{
+                btnSelect1.backgroundColor = UIColor.white
+                btnSelect1.setTitleColor(UIColor.black, for: UIControl.State.normal)
+                btnSelect2.backgroundColor = UIColor(red: 128/255, green: 194/255, blue: 179/255, alpha: 1)
+                btnSelect2.setTitleColor(UIColor.white, for: UIControl.State.normal)
+                // 여기에 버튼 클릭됐을때 밑에 퍼센트값이랑 카운트 보이고 안보이고넣기
+            }
+        } // checkButtonStatus 끝
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+} // 끝

5c68218e57315e4c7c67bc9b4ff7298c4e6089c3
mg6613 <mg6613@naver.com>
2021-02-24 15:22:33 +0900

    2021-02-24

--- WooBoo_Project/WooBoo_Project/ViewController.swift
+++ WooBoo_Project/WooBoo_Project/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblHot: UILabel!
+    @IBOutlet weak var hotPageControl: UIPageControl!
+    
+    var numcount = 10
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        hotPageControl.numberOfPages = numcount // 총 페이지 갯수
+        hotPageControl.currentPage = 0 // 현재 페이지가 몇번이냐
+        
+        hotPageControl.pageIndicatorTintColor = UIColor.green // 인디케이터 평소 색상 지정
+        hotPageControl.currentPageIndicatorTintColor = UIColor.red // 인디케이터 선택시 색상
+        lblHot.text = String(hotPageControl.currentPage + 1)
+       
     }
 
-
+   
+    @IBAction func changeHotPage(_ sender: UIPageControl) {
+        lblHot.text = String(hotPageControl.currentPage + 1)
+    }
+    
 }
 

f58f795c57775289ac0c58990a65a27e27edccdb
ParkSeMi <qkrtpa12@naver.com>
2021-02-25 14:08:18 +0900

    21.02.25 / 14:04 / 박세미 => 회원가입 정규화 추가

--- WooBoo_Project/WooBoo_Project/JoinViewController.swift
+++ WooBoo_Project/WooBoo_Project/JoinViewController.swift
@@ -14,13 +14,21 @@ class JoinViewController: UIViewController {
     @IBOutlet weak var txtCode: UITextField!
     @IBOutlet weak var txtPW: UITextField!
     @IBOutlet weak var txtPWCheck: UITextField!
-
+    @IBOutlet weak var lblpwCheck: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    
-    }// viewDidLoad
+        
+//        let pwCheck = txtPWCheck.text!
+//        let txtpw = txtPW.text!
+//        
+//        if txtpw != pwCheck{
+//            lblpwCheck.text! = "비밀번호가 일치하지 않습니다."
+//        }else{
+//            lblpwCheck.text! = "비밀번호가 일치합니다."
+//        }
+        
+    }//------------viewDidLoad
     
     // 가입하기 버튼 클릭
     @IBAction func btnJoinOk(_ sender: UIButton) {
@@ -32,38 +40,56 @@ class JoinViewController: UIViewController {
         
         if num1Check + num2Check + num3Check + num4Check != 4 {
             let nilAlert = UIAlertController(title: "경고!", message: "정확히 입력하세요.", preferredStyle: UIAlertController.Style.alert)
-            let nilAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {ACTION in
-                self.navigationController?.popViewController(animated: true)    // 현재화면 지워줌
-            })
+            let nilAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
             nilAlert.addAction(nilAction)
             present(nilAlert, animated: true, completion: nil)
         }else{
             // nil이 아닐 경우
-            
             let email = txtEmail.text!
             let pw = txtPW.text!
             
-            let joinModel = JoinModel()
-            let result = joinModel.joinItems(email: email, pw: pw)
-            
-            if result == true{
-                let nilAlert = UIAlertController(title: "완료", message: "가입 되었습니다!", preferredStyle: UIAlertController.Style.alert)
-                let nilAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {ACTION in
-                    self.navigationController?.popViewController(animated: true)    // 현재화면 지워줌
-                })
+            // 정규식 검사
+            if !isValidPassword(pwd: pw) && !isValidEmail(testStr: email){
+                let nilAlert = UIAlertController(title: "경고!", message: "이메일과 비밀번호를 확인해주세요!", preferredStyle: UIAlertController.Style.alert)
+                let nilAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
                 nilAlert.addAction(nilAction)
                 present(nilAlert, animated: true, completion: nil)
+                print("비밀번호 또는 이메일 실패")
             }else{
-                let resultAlert = UIAlertController(title: "실패", message: "에러가 발생 되었습니다.", preferredStyle: UIAlertController.Style.alert)
-                let onAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
-                resultAlert.addAction(onAction)
-                present(resultAlert, animated: true, completion: nil)
+            
+                let joinModel = JoinModel()
+                let result = joinModel.joinItems(email: email, pw: pw)
+                
+                if result == true{
+                    let nilAlert = UIAlertController(title: "완료", message: "가입 되었습니다!", preferredStyle: UIAlertController.Style.alert)
+                    let nilAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {ACTION in
+                        self.dismiss(animated: true, completion: nil)
+                    })
+                    nilAlert.addAction(nilAction)
+                    present(nilAlert, animated: true, completion: nil)
+                }else{
+                    let resultAlert = UIAlertController(title: "실패", message: "에러가 발생 되었습니다.", preferredStyle: UIAlertController.Style.alert)
+                    let onAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+                    resultAlert.addAction(onAction)
+                    present(resultAlert, animated: true, completion: nil)
+                }
             }
-            
-            
         }
     }
     
+    // 이메일 형식 검사(@ 앞에 알파벳, 숫자, 특수문자가 포함될 수 있고 @ 뒤에는 알파벳, 숫자, .뒤에는 알파벳 2자리 이상)
+    func isValidEmail(testStr:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: testStr)
+    }
+    
+    // 비밀번호 형식 검사(소문자, 대문자, 숫자 8자리 이상)
+    func isValidPassword(pwd: String) -> Bool {
+        let passwordRegEx = "^[a-zA-Z0-9]{8,}$"
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", passwordRegEx)
+        return passwordTest.evaluate(with: pwd)
+    }
     
     // 아무곳이나 눌러 softkeyboard 지우기
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -88,27 +114,27 @@ class JoinViewController: UIViewController {
         txtPWCheck.borderStyle = .none
         let border = CALayer()
         border.frame = CGRect(x: 0,
-        y: txtEmail.frame.size.height-1,
-        width: txtEmail.frame.width,
-            height: 1)
+                              y: txtEmail.frame.size.height-1,
+                              width: txtEmail.frame.width,
+                              height: 1)
         border.backgroundColor = UIColor.lightGray.cgColor
         let border1 = CALayer()
         border1.frame = CGRect(x: 0,
-        y: txtCode.frame.size.height-1,
-        width: txtCode.frame.width,
-            height: 1)
+                               y: txtCode.frame.size.height-1,
+                               width: txtCode.frame.width,
+                               height: 1)
         border1.backgroundColor = UIColor.lightGray.cgColor
         let border2 = CALayer()
         border2.frame = CGRect(x: 0,
-        y: txtPW.frame.size.height-1,
-        width: txtPW.frame.width,
-            height: 1)
+                               y: txtPW.frame.size.height-1,
+                               width: txtPW.frame.width,
+                               height: 1)
         border2.backgroundColor = UIColor.lightGray.cgColor
         let border3 = CALayer()
         border3.frame = CGRect(x: 0,
-        y: txtPWCheck.frame.size.height-1,
-        width: txtPWCheck.frame.width,
-            height: 1)
+                               y: txtPWCheck.frame.size.height-1,
+                               width: txtPWCheck.frame.width,
+                               height: 1)
         border3.backgroundColor = UIColor.lightGray.cgColor
         txtEmail.layer.addSublayer((border))
         txtCode.layer.addSublayer((border1))
@@ -123,15 +149,15 @@ class JoinViewController: UIViewController {
         txtPW.textColor = UIColor.black
         txtPWCheck.textColor = UIColor.black
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }

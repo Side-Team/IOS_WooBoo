438c26c7b240a73b0e066f90c6bed7fc2a97c25a
이민우 <tnctis21@naver.com>
2021-03-06 00:04:20 +0900

    2021-03-05 / 00:03 / 이민우 =>
    1. ViewPagerController.swift 생성
    2. 우부판 hot, new에서 이동 시 시퀀스가 맞지 않던 이슈 해결

--- WooBoo_Project/WooBoo_Project/Models/LMW_InsertModel.swift
+++ WooBoo_Project/WooBoo_Project/Models/LMW_InsertModel.swift
@@ -11,8 +11,8 @@ class LMW_InsertModel: NSObject{
     
     var urlPath = "http://127.0.0.1:8080/ios_jsp/InsertQuestion.jsp"
     
-    func insertItems(qTitle: String, qSelection1: String, qSelection2: String, qSelection3: String, qSelection4: String, qSelection5: String, qCategory: Int, qImageFileName1: String, qImageFileName2: String, qImageFileName3: String, qImageFileName4: String, qImageFileName5: String) -> Bool{
-        var result: Bool = true
+    func insertItems(qTitle: String, qSelection1: String, qSelection2: String, qSelection3: String, qSelection4: String, qSelection5: String, qCategory: Int, qImageFileName1: String, qImageFileName2: String, qImageFileName3: String, qImageFileName4: String, qImageFileName5: String){
+
         let urlAdd = "?user_uSeqno=\(Share.uSeqno)&qTitle=\(qTitle)&qSelection1=\(qSelection1)&qSelection2=\(qSelection2)&qSelection3=\(qSelection3)&qSelection4=\(qSelection4)&qSelection5=\(qSelection5)&qCategory=\(qCategory)&qImageFileName1=\(qImageFileName1)&qImageFileName2=\(qImageFileName2)&qImageFileName3=\(qImageFileName3)&qImageFileName4=\(qImageFileName4)&qImageFileName5=\(qImageFileName5)"
         
         urlPath = urlPath + urlAdd
@@ -28,15 +28,11 @@ class LMW_InsertModel: NSObject{
         let task = defaultSession.dataTask(with: url){(data, response, error) in
             if error != nil{
                 print("Failed to insert data")
-                result = false
             }else{
                 print("Data is inserted!")
-                result = true
             }
         }
         task.resume()
-        print("insertItems bool : \(result)")
-        return result
     }
     
     func insert_registerT(questions_qSeqno : Int){
